56d1ef33e7a82383675296667bafc1d56f6f02ed
Martin Porrès <martin.porres@epitech.eu>
2017-04-30 19:33:44 +0200

    Part 1 almost done

--- maze_factory.rb
+++ maze_factory.rb
@@ -4,52 +4,145 @@ Mathieu Bourmaud - 19941124-P335
 Martin Porrès - 19940926-P170
 =end
 
+require './game'
+
 class MazeFactory
   def maze_game(length, height)
     return Maze.new(length, height)
   end
 
-  def make_room(x, y)
-    return Room.new(x, y)
+  def make_room
+    return Room.new
   end
 
-  def make_door(r1, r2)
-    return Door.new(r1, r2)
+  def make_door(room)
+    return Door.new(room)
   end
 
-  def make_wall(r1, r2)
-    return Wall.new(r1, r2)
+  def make_wall
+    return Wall.new()
   end
+end
+
+class Monster
+  @challenge = Game.new() #this is the RPS game
 end
 
 class Wall
-  def initialize(r1, r2)
-    @r1, @r2 = r1, r2
-  end
 end
 
 class Door
-  def initialize(r1, r2)
-    @r1, @r2 = r1, r2
+  def initialize(room)
+    # @room is the room that the door leads to
+    @room = room
+    <<-DOC
+    case o1
+      when 'n'
+        @r1.set_elem('n', 'w')
+        @r2.set_elem('s', 'w')
+      when 's'
+        @r1.set_elem('s', 'w')
+        @r2.set_elem('n', 'w')
+      when 'e'
+        @r1.set_elem('e', 'w')
+        @r2.set_elem('w', 'w')
+      when 'w'
+        @r1.set_elem('w', 'w')
+        @r2.set_elem('e', 'w')
+      end
+    DOC
   end
 end
 
 class Room
-  def initialize(x, y)
-    @x, @y = x, y
+  @orientation = {'n' => Wall.new, 's' => Wall.new, 'e' => Wall.new, 'w' => Wall.new}
+  @content = nil
+
+  # set the element (type = w or d) on the wall (o1 = n, s, e, w)
+  def set_elem(type, o1='n')
+    @orientation[o1]= type
   end
+
+  # get the element (return = w or d) on the wall (o1 = n, s, e, w)
+  def get_elem(o1)
+    return @orientation[o1]
+  end
+
+  def set_content(type)
+    @content = type
+  end
+
+  def get_content(type)
+    return type
+  end
+
 end
 
 class Maze
   def initialize(length, height)
     @length, @height = length, height
+    @map = Array.new(length){Array.new(height, Room.new)}
+  end
+
+  def set_room(room, x, y)
+    @map[x, y] = room
+  end
+
+  def get_room(x, y)
+    return @map[x, y]
   end
 end
 
 def make_maze(x_size, y_size)
   factory = MazeFactory.new()
-  m = factory.maze_game(x_size, y_size)
+  @m = factory.maze_game(x_size, y_size)
+  for x in 0..x_size
+    for y in 0..y_size
+      #creating a door to a random room
+      rand_x = rand(x_size)
+      rand_y = rand( y_size)
+      room = @m.get_room(x, y)
+      rand_room = @m.get_room(rand_x, rand_y)
+      ori = ['n', 's', 'e', 'w'].sample()
+      op_ori = get_opposed_orientation(ori)
+      #create an enemy (1 chance out of 10)
+      if 0 == rand(10)
+        room.set_content(Monster.new)
+      end
+      #check if rooms already have a door
+      if !(room.get_elem(ori).is_a?(Door) && !rand_room.get_elem(op_ori).is_a?(Door)
+        door = factory.make_door(rand_room)
+        door2 = factory.make_door(room)
+        room.set_elem(door, ori)
+        rand_room.set_elem(door2, op_ori)
+      end
+    end
+  end
+end
+
+def get_opposed_orientation(orientation)
+  case orientation
+    when 'n'
+      return 's'
+    when 's'
+      return 'n'
+    when 'e'
+      return 'w'
+    when 'w'
+      return 'e'
+  end
+  return 'n'
 end
 
 make_maze(20, 20)
+
+#définir la tresor room
+# spawn le joueur en [0,0]
+#créer boucle de jeu
+  # lire la salle:
+  # si salle du trésor, win
+  # si monstre -> demander combat (L, F)
+  # sinon décrire la salle (portes)
+  # demander quelle porte prendre (N, S, E, W)
+#fin boucle
 
